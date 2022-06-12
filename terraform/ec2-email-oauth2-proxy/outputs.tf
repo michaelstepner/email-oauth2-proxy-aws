9df3117c00a9457ae32c50687462a2a731c83d6a
Michael Stepner <software@michaelstepner.com>
2022-06-12 12:00:23 -0400

    Use Elastic IP to output public IP

--- terraform/ec2-email-oauth2-proxy/outputs.tf
+++ terraform/ec2-email-oauth2-proxy/outputs.tf
@@ -1,4 +1,4 @@
-output "instance_public_ip" {
+output "public_ip" {
   description = "Public IP of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
+  value       = aws_eip.app_server.public_ip
 }
