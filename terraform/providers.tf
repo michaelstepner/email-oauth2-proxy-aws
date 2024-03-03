813834ae608231e9ced5dc5a4d96dbec8f315bc2
Michael Stepner <software@michaelstepner.com>
2024-03-03 14:38:42 -0500

    Upgrade TF and TF provider versions
    Also pin TF at last open source version

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,19 +2,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.12.0"
+      version = "~> 5.39.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0.4"
+      version = "~> 4.0.5"
     }
     acme = {
       source  = "vancluever/acme"
-      version = "~> 2.16.1"
+      version = "~> 2.20.2"
     }
   }
 
-  required_version = "~> 1.5.4"
+  required_version = "= 1.5.7"
 }
 
 provider "aws" {
